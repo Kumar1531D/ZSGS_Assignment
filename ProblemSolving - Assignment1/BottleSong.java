74ede3b089e7e5eeaf21a739e650c3327fec4fa2
Kumar1531D <164643227+Kumar1531D@users.noreply.github.com>
2024-11-04 08:07:33 +0530

    Update BottleSong.java

--- ProblemSolving - Assignment1/BottleSong.java
+++ ProblemSolving - Assignment1/BottleSong.java
@@ -11,7 +11,7 @@ public class BottleSong
     {
     	String[] num = {"No", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"};
     	StringBuilder lyrics=new StringBuilder();
-    	for (int i = 10; i > 0; i--) // from 10 bottles, take one by one until reaches zero 
+    	for (int i = 10; i > 0; i--)
     	{
             lyrics.append(num[i] + " green bottles hanging on the wall,\n");
             lyrics.append(num[i] + " green bottles hanging on the wall,\n");
@@ -22,57 +22,3 @@ public class BottleSong
     	return lyrics.toString();
     }
 }
-/*
-OUTPUT
-
-Ten green bottles hanging on the wall,
-Ten green bottles hanging on the wall,
-And if one green bottle should accidentally fall,
-There'll be nine green bottles hanging on the wall.
-
-Nine green bottles hanging on the wall,
-Nine green bottles hanging on the wall,
-And if one green bottle should accidentally fall,
-There'll be eight green bottles hanging on the wall.
-
-Eight green bottles hanging on the wall,
-Eight green bottles hanging on the wall,
-And if one green bottle should accidentally fall,
-There'll be seven green bottles hanging on the wall.
-
-Seven green bottles hanging on the wall,
-Seven green bottles hanging on the wall,
-And if one green bottle should accidentally fall,
-There'll be six green bottles hanging on the wall.
-
-Six green bottles hanging on the wall,
-Six green bottles hanging on the wall,
-And if one green bottle should accidentally fall,
-There'll be five green bottles hanging on the wall.
-
-Five green bottles hanging on the wall,
-Five green bottles hanging on the wall,
-And if one green bottle should accidentally fall,
-There'll be four green bottles hanging on the wall.
-
-Four green bottles hanging on the wall,
-Four green bottles hanging on the wall,
-And if one green bottle should accidentally fall,
-There'll be three green bottles hanging on the wall.
-
-Three green bottles hanging on the wall,
-Three green bottles hanging on the wall,
-And if one green bottle should accidentally fall,
-There'll be two green bottles hanging on the wall.
-
-Two green bottles hanging on the wall,
-Two green bottles hanging on the wall,
-And if one green bottle should accidentally fall,
-There'll be one green bottles hanging on the wall.
-
-One green bottles hanging on the wall,
-One green bottles hanging on the wall,
-And if one green bottle should accidentally fall,
-There'll be no green bottles hanging on the wall.
-
-*/
